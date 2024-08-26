ff3a708062e522793cb0fb42924910b304af01d0
Guilherme <112200498+GuilhermeArmstrong@users.noreply.github.com>
2024-08-26 11:39:13 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,67 @@
+export const perguntas = [
+    {
+        enunciado: "Pensando no bem estar do planeta e de seus habitantes, inúmeros metodos foram criados para preserva-lo por mais tempo, trocar as energias não renováveis por energias renováveis como a energia eolica é uma delas, qual a sua opinião sobre isso?",
+        alternativas: [
+            {
+                texto: "Concordo",
+                afirmacao: "Muito bem, realmente é uma opção conciente e correta."
+            },
+            {
+                texto: "Discordo",
+                afirmacao: "Porque? Certamente se isso acontecer o planeta ficaria a salvo por muito mais tempo."
+            }
+        ]
+    },
+    {
+        enunciado: "Suas ações estão condizentes com seus atos?",
+        alternativas: [
+            {
+                texto: "Sim",
+                afirmacao: "Você contribuindo com isso é importantissímo para reverter a situação problematica estamos"
+            },
+            {
+                texto: "Não",
+                afirmacao: "Mas é uma pena que você nao esteja contribuindo para reverter esssa situação alarmante que estamos"
+            }
+        ]
+    },
+    {
+        enunciado: "Na sua casa alguem contribui para salvar o planeta?",
+        alternativas: [
+            {
+                texto: "Sim",
+                afirmacao: "Que bom, isso é muito importante"
+            },
+            {
+                texto: "Não",
+                afirmacao: "Então o que acha de incentiva-los a ajudar mais o nosso querido planeta?"
+            }
+        ]
+    },
+    {
+        enunciado: "Você acha que pode mudar seus habitos para que sejam mais saudaveis para o planeta? ou tentar dicemina-los para mais pessoas?",
+        alternativas: [
+            {
+                texto: "Sim",
+                afirmacao: "Perfeito!!!"
+            },
+            {
+                texto: "talvez...",
+                afirmacao: "Uma dica é começar com pequenas coisas"
+            }
+        ]
+    },
+    {
+        enunciado: "Esse mini questionario te ajudou a refletir sobre esse assunto importante?",
+        alternativas: [
+            {
+                texto: "Sim, obrigado(a)",
+                afirmacao: "Espero sempre poder ajuda com reflexões saudavéis."
+            },
+            {
+                texto: "Não, melhore-o na proxima vez",
+                afirmacao: "Sinto muito, procurarei aprimora-lo."
+            }
+        ]
+    },
+];
